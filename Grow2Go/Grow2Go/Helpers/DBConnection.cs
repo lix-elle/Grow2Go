c2b1ef985a8ac2610bdad5587b3e0d9530ae5658
Lix <lynsymariesoronio05@gmail.com>
2026-04-08 19:29:18 +0800

    Add DBConnection helper class

---
+++ Grow2Go/Grow2Go/Helpers/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Grow2Go.Helpers 
+{
+    public class DBConnection
+    {
+        private static readonly string connectionString = "Server=localhost;Port=3306;Database=grow2go;Uid=root;Pwd=grow2go123";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public static bool TestConnection() 
+        {
+            try
+            {
+                using (var conn = GetConnection())
+                {
+                    conn.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection Failed: " + ex.Message);
+            }
+        }
+    }
+}
